b2e66334ed6d98e38819cd5c0edbf05f80135464
virak2312 <93199687+virak2312@users.noreply.github.com>
2022-02-12 10:40:21 +0700

    Add files via upload

---
+++ Sannin900Hub.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 4520749081 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sannin9000/scripts/main/kinglegacy.lua"))()
+elseif game.PlaceId == 537413528 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sannin9000/scripts/main/buildaboatfortreasure.lua"))()
+elseif game.PlaceId == 71315343 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sannin9000/scripts/main/dbrage.lua"))()
+elseif game.PlaceId == 5835263912 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sannin9000/scripts/main/opm3.lua"))()
+end   
+
+setclipboard("https://discord.gg/tCNEg3Naju")
